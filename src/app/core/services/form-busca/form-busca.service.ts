56520d79d8bc9040f31c042d46edb9ad0fedbc64
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-10-24 09:59:06 -0300

    alteração modal

--- src/app/core/services/form-busca/form-busca.service.ts
+++ src/app/core/services/form-busca/form-busca.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipSelectionChange } from '@angular/material/chips';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormBuscaService {
   formBusca: FormGroup;
-  constructor() { 
-   
+  constructor(private dialog: MatDialog) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
-    })
+      tipo: new FormControl('Econômica'),
+    });
   }
 
-  obterControle(nome:string): FormControl {
+  obterControle(nome: string): FormControl {
     const control = this.formBusca.get(nome);
     if (!control) {
       throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
     return control as FormControl;
   }
-  
+  alteraTipo (event: MatChipSelectionChange, tipo: string){
+    if (event.selected){
+      this.formBusca.patchValue({
+        tipo,
+      })
+    }
+    console.log('pasagem', tipo)
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%',
+    });
+  }
 }
--- src/app/shared/formulario-busca/formulario-busca.component.ts
+++ src/app/shared/formulario-busca/formulario-busca.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalComponent } from '../modal/modal.component';
+
 import { FormBuscaService } from './../../core/services/form-busca/form-busca.service';
 
 @Component({
@@ -9,12 +8,8 @@ import { FormBuscaService } from './../../core/services/form-busca/form-busca.se
   styleUrls: ['./formulario-busca.component.scss'],
 })
 export class FormularioBuscaComponent {
-  constructor(public dialog: MatDialog, 
+  constructor(
     public formBuscaService: FormBuscaService) {}
 
-  openDialog() {
-    this.dialog.open(ModalComponent,{
-      width: '50%',
-    });
-  }
+
 }
