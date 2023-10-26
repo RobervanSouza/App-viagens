8534788b2775568cf91f02c82e7b8200bab7ad36
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-10-25 22:10:42 -0300

    atualização de cadastro

--- src/app/core/services/CadastroUser/cadastro-user.service.ts
+++ src/app/core/services/CadastroUser/cadastro-user.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { PessoaUsuaria } from '../../types/type';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CadastroUserService {
+  private apiUrl: string = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria> {
+    return this.http.post<PessoaUsuaria>(
+      `${this.apiUrl}/auth/cadastro`,
+      pessoaUsuaria
+    );
+  }
 }
