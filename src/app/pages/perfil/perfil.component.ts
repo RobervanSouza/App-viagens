427d0c2ed86fafd70b8593e62386bbec73b622b5
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-02 08:09:45 -0300

    exibindo dados tela de perfil

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { CadastroUserService } from 'src/app/core/services/CadastroUser/cadastro-user.service';
+import { FormularioCadastroService } from 'src/app/core/services/cadastrar/formulario-cadastro.service';
 import { TokenService } from 'src/app/core/services/token/token.service';
 import { PessoaUsuaria } from 'src/app/core/types/type';
 
@@ -16,10 +18,12 @@ export class PerfilComponent implements OnInit {
   token= '';
   nome = '';
   cadastro!: PessoaUsuaria;
+  formGrup!: FormGroup<any> | null;
 
   constructor(
    private tokenService: TokenService,
-   private cadastroService: CadastroUserService
+   private cadastroService: CadastroUserService,
+   private formService: FormularioCadastroService
 
   ) {}
   ngOnInit(): void {
@@ -27,10 +31,24 @@ export class PerfilComponent implements OnInit {
     this.cadastroService.buscarCadastro(this.token).subscribe(cadastro =>{
       this.cadastro = cadastro;
       this.nome = this.cadastro.nome
+      this.carregarFormulario();
     })
   }
 
- 
+  carregarFormulario(){
+   this.formGrup = this.formService.getCadastro();
+    this.formGrup?.patchValue({
+    nome: this.cadastro.nome,
+    nascimento: this.cadastro.nascimento,
+    cpf: this.cadastro.cpf,
+    telefone: this.cadastro.telefone,
+    email: this.cadastro.email,
+    senha: this.cadastro.senha,
+    cidade: this.cadastro.cidade,
+    estado: this.cadastro.estado,
+    genero: this.cadastro.genero,
+    })
+  }
 
   deslogar () {
     console.log('logooooout')
