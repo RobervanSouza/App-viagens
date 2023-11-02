0c78544977fc80d395b0d93d8d0baab3715c8d06
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-02 08:20:53 -0300

    editando dados do usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CadastroUserService } from 'src/app/core/services/CadastroUser/cadastro-user.service';
 import { FormularioCadastroService } from 'src/app/core/services/cadastrar/formulario-cadastro.service';
 import { TokenService } from 'src/app/core/services/token/token.service';
 import { PessoaUsuaria } from 'src/app/core/types/type';
+import { UsuarioService } from './../../core/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -23,8 +25,9 @@ export class PerfilComponent implements OnInit {
   constructor(
    private tokenService: TokenService,
    private cadastroService: CadastroUserService,
-   private formService: FormularioCadastroService
-
+   private formService: FormularioCadastroService,
+   private rotas: Router,
+   private userService: UsuarioService,
   ) {}
   ngOnInit(): void {
     this.token = this.tokenService.retornarToken();
@@ -51,9 +54,30 @@ export class PerfilComponent implements OnInit {
   }
 
   deslogar () {
-    console.log('logooooout')
+   this.userService.logout();
+   this.rotas.navigate(['/login']);
   }
   atualizar(){
-    console.log('cadastro atualizado')
+    const dadosAtualizado = {
+      nome: this.formGrup?.value.nome,
+      nascimento: this.formGrup?.value.nascimento,
+      cpf: this.formGrup?.value.cpf,
+      telefone: this.formGrup?.value.telefone,
+      email: this.formGrup?.value.email,
+      senha: this.formGrup?.value.senha,
+      cidade: this.formGrup?.value.cidade,
+      estado: this.formGrup?.value.estado,
+      genero: this.formGrup?.value.genero,
+    }
+
+    this.cadastroService.editarCadastro(dadosAtualizado, this.token).subscribe({
+      next:()=>{
+        alert("Cadastro atualizado com sucesso")
+        this.rotas.navigate(["/"])
+      },
+      error(err) {
+          console.log(err, "Erro ao atualizar cadastro!!!")
+      },
+    })
   }
 }
